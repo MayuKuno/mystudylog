48bf698a99700e29b78362c6fc01de5a20eec28c
mayukuno <kunomayu1006@gmail.com>
2024-01-06 15:08:16 +0900

    added 1_two-sum.ts

---
+++ typescript/1_two-sum.ts
@@ -0,0 +1,20 @@
+// Description
+// https://leetcode.com/problems/two-sum/description/
+
+
+// My Answer
+// `| undefined` is for testing
+function twoSum(nums: number[], target: number): number[] | undefined {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+      let currentNumber = nums[i];
+      let diff = target - currentNumber;
+      if (map.has(diff)) {
+          return [map.get(diff), i];
+      }
+      map.set(currentNumber, i);
+  }
+};
+
+const output = twoSum([3,2,4],6)
+console.log(output)
